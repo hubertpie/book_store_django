654e2f9e6df4c22e69c88375b1b78e44c0613e2c
hubertpie <hubert.pietron95@gmail.com>
2020-09-26 17:19:56 +0200

    added book and category model

---
+++ book_store/shop/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+
+class Category(models.Model):
+
+	name = models.CharField(max_length=200, db_index=True)
+	slug = models.SlugField(max_length=200,
+							db_index=True,
+							unique=True)
+
+	class Meta:
+		ordering = ('name',)
+		verbose_name = 'category'
+		verbose_name_plural = 'categories'
+
+	def __str__(self):
+		return self.name
+
+
+class Book(models.Model):
+
+	category = models.ForeignKey(Category,
+								related_name='books',
+								on_delete=models.CASCADE)
+	title = models.CharField(max_length=200, db_index=True)
+	slug = models.SlugField(max_length=200, db_index=True)
+	author = models.CharField(max_length=100)
+	publishing_house = models.CharField(max_length=100)
+	image = models.ImageField(upload_to='books/%Y/%m/%d', blank=True)
+	description = models.TextField(blank=True)
+	price = models.DecimalField(max_digits=10, decimal_places=2)
+	available = models.BooleanField(default=True)
+	added_to_store = models.DateTimeField(auto_now_add=True)
+	release_date = models.DateTimeField()
+	pages = models.IntegerField()
+
+	class Meta:
+		ordering = ('title', )
+
+	def __str__(self):
+		return self.title
