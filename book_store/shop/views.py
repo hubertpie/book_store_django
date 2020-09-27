5547893cd005cdb877b305ce0479383cd25347e1
hubertpie <hubert.pietron95@gmail.com>
2020-09-27 10:35:38 +0200

    home page shows a list of books

--- book_store/shop/views.py
+++ book_store/shop/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from .models import Category, Book
 
 def book_list(request):
-	return render(request, 'shop/book/list.html')
+	books = Book.objects.all()
+	return render(request, 'shop/book/list.html', {'books': books})
