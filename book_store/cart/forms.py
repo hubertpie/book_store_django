4cbad4ab3724897a665bc70906c546712643dcea
hubertpie <hubert.pietron95@gmail.com>
2020-10-06 06:27:01 +0200

    added cart context processor

--- book_store/cart/forms.py
+++ book_store/cart/forms.py
@@ -1,7 +1,5 @@
 from django import forms
 
-BOOK_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 6)]
-
 class CartAddBookForm(forms.Form):
     quantity = 1
     update = forms.BooleanField(required=False,
