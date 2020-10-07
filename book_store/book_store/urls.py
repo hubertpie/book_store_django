8af58a4f385d08828155854643b973b6c78d6943
hubertpie <hubert.pietron95@gmail.com>
2020-10-07 06:04:32 +0200

    added form, url, template for placing order

--- book_store/book_store/urls.py
+++ book_store/book_store/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('orders/', include('orders.urls', namespace='orders')),
     path('shop/', include('shop.urls', namespace='shop')),
     path('cart/', include('cart.urls', namespace='cart'))
 ]
