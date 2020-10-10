0c5b1607ec23500923ec87746c8e068dc6c3ea8c
hubertpie <hubert.pietron95@gmail.com>
2020-10-10 17:29:59 +0200

    deleted celery from project

--- book_store/orders/views.py
+++ book_store/orders/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
-#from .tasks import order_created
 
 def order_create(request):
     cart = Cart(request)
@@ -17,8 +16,6 @@ def order_create(request):
                                         quantity=item['quantity'])
 
             cart.clear()
-            # Running asynchronous task
-            #order_created.delay(order.id)
             return render(request, 'orders/order/created.html', {'order': order})
         
     else:
