52aaf3f45db7219c77ae531798d6b8deb9016fb5
hubertpie <hubert.pietron95@gmail.com>
2020-10-20 06:12:10 +0200

    deleted unnecessary code

--- book_store/account/forms.py
+++ book_store/account/forms.py
@@ -1,6 +1,16 @@
 from django import forms
+from django.contrib.auth.models import User
 
-class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
-    
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='password', widget=forms.PasswordInput)
+    password = forms.CharField(label='password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']
+            raise forms.ValidationError('Please repeat your password')
+        return cd['password2']
--- book_store/account/views.py
+++ book_store/account/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
 
