7597905394b12252e8f8d1d0053de2c4dbb747cc
hubertpie <hubert.pietron95@gmail.com>
2020-10-31 13:46:06 +0100

    Committed some changes

--- book_store/book_store/settings.py
+++ book_store/book_store/settings.py
@@ -130,6 +130,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 CART_SESSION_ID = 'cart'
+
 LOGIN_REDIRECT_URL = '/shop'
 LOGOUT_REDIRECT_URL = '/shop'
 
